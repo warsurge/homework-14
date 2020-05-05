ce1ae55e2d026658932c01a08b5ec0870abfa8c4
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-05 17:43:18 -0500

    added database info

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,3 +1,41 @@
-const mongoose = recquire("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const workout = newSchema({
+day: {
+    type:Date,
+    default: () => new Date()
+},
+exercises: [{
+    type: {
+        type: String,
+        trim: true,
+        required: "Enter an excercise type"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter an excercise name"
+    },
+    duration: {
+        type: Number,
+        required: "Enter a duration time"
+    },
+    weight: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    distance: {
+        type: Number
+    }
+}]
+});
+
+const Workout = mongoose.model("Workout", workout);
+
+module.exports = Workout;
