fe623286aca368bf98dd58419d9dc2e549ebac14
Sergio Bracamontes <sbracamontes458@cardinal.wju.edu>
2020-05-07 15:03:00 -0500

    trying to connect to heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 8080;
 
 const db = require("./models");
 
 const app = express();
+app.use(compression());
 
 app.use(logger("dev"));
 
@@ -15,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://warsurge:pimp15@ds255857.mlab.com:55857/heroku_ssg54hm5", { useNewUrlParser: true });
 
 
 require("./routes/apiRoutes")(app);
